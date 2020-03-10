9c470189827100950750bbe7755b2ce71c80b6fb
Jose <jrs3@gatech.edu>
2020-03-10 17:29:46 -0400

    python file

--- networkProjectOne.py
+++ networkProjectOne.py
@@ -1,10 +1,11 @@
 def getword(wordList):
     for word in wordList:
-        if
+        if (word == like):
+            print(word)
 
-stringWord="we like apple. We don't like water"
 
-data = stringWord.split() #split string into a list
+stringWord = "we like apple. We don't like water"
 
+data = stringWord.split()  # split string into a list
 
 getword(data)
