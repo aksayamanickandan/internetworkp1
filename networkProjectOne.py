1a069045413115ddc52224b8239224b5d6e34e92
Jose <jrs3@gatech.edu>
2020-03-10 18:03:27 -0400

    update

--- networkProjectOne.py
+++ networkProjectOne.py
@@ -1,24 +1,26 @@
-likeList=[]
-notLikeList=[]
+likeList = []
+notLikeList = []
+
+
 def getword(wordList):
-
-    for ind ,word in enumerate(wordList):
-        if (word == "like" and wordList[ind-1] != "don't" ):
+    for ind, word in enumerate(wordList):
+        if (wordList[ind - 1] != "don't" and word == "like" and wordList[ind + 2] == "and"):
             global likeList
-            likeList.append(wordList[ind+1])
-        elif(word=="don't" ):
+            likeList.append(wordList[ind + 1])
+            likeList.append(wordList[ind + 3])
+        elif (wordList[ind - 1] != "don't" and word == "like"):
+            likeList.append(wordList[ind + 1])
+
+        elif (word == "don't"):
             global notLikeList
-            notLikeList.append(wordList[ind+2])
+            notLikeList.append(wordList[ind + 2])
 
 
-
-
-
-stringWord = "we like apple. We don't like water" #this is our input
+stringWord = "we like apple and corona . We don't like water and corona"  # this is our input
 
 data = stringWord.split()  # split string into a list
 
-getword(data)# this function takes in the list and searches for key words
+getword(data)  # this function takes in the list and searches for key words
 
 print((notLikeList))
 print((likeList))
