589a1e35971a8600a5144dc28bbbcc8eb8d1f07a
Jose <jrs3@gatech.edu>
2020-03-10 17:39:05 -0400

    python file

--- networkProjectOne.py
+++ networkProjectOne.py
@@ -1,11 +1,17 @@
+likeList=[]
+notLikeList=[]
 def getword(wordList):
-    for word in wordList:
-        if (word == like):
-            print(word)
+    global likeList
+    global notLikeList
+    for ind ,word in enumerate(wordList):
+        if (word == "like"):
+            likeList.append(wordList[ind+1])
+    print(likeList)
+
 
 
 stringWord = "we like apple. We don't like water"
 
 data = stringWord.split()  # split string into a list
 
-getword(data)
+getword(likeList)
